0e86626453e7d24a0a401a3c2d458a33be739d09
zhaoyang1221 <164258541@qq.com>
2016-10-14 11:43:50 +0800

    更新数据集获取方式后相应的修改

--- main.R
+++ main.R
@@ -1,13 +1,11 @@
-#读取数据
-Kobe <- read.csv("data/Kobe Bryant.csv")
-LeBron <- read.csv("data/LeBron James.csv")
-Curry <- read.csv("data/Stephen Curry.csv")
-
+#读取数据全部的csv文件名
 filenames <- list.files("datasets", pattern = ".csv")
 
-#得到球员数据集
+#得到球员数据集,并保留只需要的维度
+myvars <- c("G", "MP", "FG", "FGA", "X3P", "X3PA", "FT", "FTA", "ORB", "DRB", "AST", "STL", "BLK", "TOV", "PF", "PTS")
 datalist <- lapply(filenames, function(name){
-  read.csv(paste("datasets/", name, sep = ""))
+  out <- read.csv(paste("datasets/", name, sep = ""))
+  out <- out[myvars]
 })
 
 #球员姓名列表
@@ -17,42 +15,42 @@ playernames <- lapply(filenames, function(name){
 #为球员数据集命名
 names(datalist) = playernames
 
-
-#数据预处理,保留需要的维度
-myvars <- c("G", "MP", "FG", "FGA", "X3P", "X3PA", "FT", "FTA", "ORB", "DRB", "AST", "STL", "BLK", "TOV", "PF", "PTS")
-Kobe <- Kobe[myvars]
-LeBron <- LeBron[myvars]
-
 #计算每一维度上的标准差
-sdOfKobe <- apply(Kobe, 2, sd)
-sdOfLeBron <- apply(LeBron, 2, sd)
+sdOfDatalist <- lapply(datalist, function(data){
+  apply(data, 2, sd)
+})
 
 #第一种不确定性计算公式（每一维度的标准差之和），
 #参数为每个维度的标准差
-uncertaintyFunc1 <- function(x){
-  return(sum(x))
+uncertaintyFunc1 <- function(sdOfDatalist){
+  result <- lapply(sdOfDatalist, function(sdOfPlayer){
+    sum(sdOfPlayer)
+  })
 }
 
 #计算第一种不确定性
-uncertaintyOfKobe <- uncertaintyFunc1(sdOfKobe)
-uncertaintyOfLeBron <- uncertaintyFunc1(sdOfLeBron)
+uncertaintyList1<- uncertaintyFunc1(sdOfDatalist)
 
 #计算协方差矩阵
-covOfKobe <- cov(Kobe)
-covOfLeBron <- cov(LeBron)
+covMatrixList <- lapply(datalist, function(data){
+  cov(data)
+})
 
 #计算协方差矩阵的特征值
-evOfKobe <- eigen(covOfKobe, only.values = TRUE)$values
-evOfLeBron <- eigen(covOfLeBron, only.values = TRUE)$values
+evList <- lapply(covMatrixList, function(covMatrix){
+  eigen(covMatrix, only.values = TRUE)$values
+})
 
 #第二种不确定性计算方程，
-#参数为协方差矩阵的特征值
+#参数为单个球员协方差矩阵的特征值
 uncertaintyFunc2 <- function(x){
   d <- length(myvars) / 2
   result <- pi^d / gamma(d+1) * prod(sqrt(x), na.rm = TRUE)
   return(result)
 }
 
-uncertaintyOfKobe2 <- uncertaintyFunc2(evOfKobe)
-uncertaintyOfLeBron2 <- uncertaintyFunc2(evOfLeBron)
+uncertaintyList2 <- lapply(evList, function(ev){
+  uncertaintyFunc2(ev)
+})
+
 
