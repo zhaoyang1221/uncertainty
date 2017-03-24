69a7d7823ee68319f7cbf6cd670de110972ccad6
zhaoyang1221 <164258541@qq.com>
2017-03-24 15:28:29 +0800

    子空间计算和不确定性计算

---
+++ foodSubspace.R
@@ -0,0 +1,50 @@
+food <- read.csv("food.csv")
+row.names(food) <- food[,1]
+food <- food[,-1]
+
+library(subspace)
+foodClusters <- CLIQUE(food,xi=40,tau=0.15)
+
+foodClusters[[1]]$subspace
+foodClusters[[1]]$objects
+
+#子空间mds图
+library(ggplot2)
+require(cowplot)
+plots <- NULL
+for (i in 1:20) {
+  subspaceOfFood <- food[foodClusters[[i]]$objects,]
+  d <- dist(subspaceOfFood)
+  fit <- cmdscale(d, k=2, eig = TRUE)
+  x <- fit$points[,1]
+  y <- fit$points[,2]
+  
+  plots[[i]] <- ggplot(data.frame(x,y), aes(x,y)) + geom_point(size=1 ,color = "blue") + xlab("") + ylab("");
+}
+plot_grid(plotlist = plots)
+
+#子空间不确定性
+uncertaintyList <- lapply(foodClusters, function(cluster){
+  subOfFood <- food[, cluster$subspace]
+  if (typeof(subOfFood) == "list") {
+    multiDimensionUncertaintyFun(subOfFood)
+  } else {
+    OneDimensionUncertaintyFun(subOfFood)
+  }
+  
+})
+
+#只有一个维度的子空间，利用标准差作为不确定性
+OneDimensionUncertaintyFun <- function(x) {
+  sd(x)
+}
+
+#多维子空间不确定性计算
+multiDimensionUncertaintyFun <- function(x) {
+  covOfFood <- cov(x)
+  eigenValues <- eigen(covOfFood, only.values = TRUE)$values
+  d <- ncol(x) / 2
+  result <- pi^d / gamma(d+1) * prod(sqrt(eigenValues), na.rm = TRUE)
+  return(result)
+}
+
--- subspace.R
+++ subspace.R
@@ -1,11 +1,9 @@
 cars <- read.csv("cars.csv")
+row.names(cars) <- cars[,1]
+cars <- cars[,-1]
 
-cars1<- subset(cars, select = -name)
-cars1<- sapply(cars1, function(s){
-  as.numeric(s)
-})
-cars1<-as.data.frame(cars1)
 
 library(subspace)
 carsCluters <- CLIQUE(cars1,xi=40,tau = 0.08)
-plot(carsCluters,cars1)
+
+library(cowplot)
