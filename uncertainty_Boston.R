a61a3ff9ea0d2b94f9ac22cd480d96d1ae3ac4c2
zhaoyang1221 <164258541@qq.com>
2017-05-04 22:51:01 +0800

    更新了抽样方式，香龙熵计算还有问题

--- uncertainty_Boston.R
+++ uncertainty_Boston.R
@@ -2,3 +2,12 @@ require(MASS)
 
 library(fpc)
 library(cluster)
+data("Boston")
+Boston.scale <- scale(Boston)
+Boston.pamk <- pamk(Boston.scale)
+Boston.pam <- pam(Boston.diss, Boston.pamk$nc, diss = TRUE)
+autoplot(Boston.pamk$pamobject, frame = TRUE, frame.type = "norm")
+Boston.sil <- silhouette(Boston.pamk$pamobject)
+plot(Boston.sil)
+summary(Boston)
+
