adcc932df4a4d54aeadf075fbc9975089612e981
zhaoyang1221 <164258541@qq.com>
2017-07-25 16:57:29 +0800

    output json files

--- uncertainty_Boston.R
+++ uncertainty_Boston.R
@@ -2,7 +2,10 @@ require(MASS)
 data(Boston)
 library(fpc)
 library(cluster)
+library(ggplot2)
 library(ggfortify)
+
+Boston <- Boston[,-c(4,9)]
 
 Boston.scale <- scale(Boston, center = TRUE, scale = TRUE)
 
@@ -54,33 +57,25 @@ temp1[[5]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.2))
 temp1[[6]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.26))
 temp1[[7]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.31))
 temp1[[8]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.35))
-temp1[[9]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.4))
-temp1[[10]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.42))
-temp1[[11]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.43))
-temp1[[12]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.45))
-temp1[[13]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.467))
-temp1[[14]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.473))
+temp1[[9]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.38))
+temp1[[10]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.40))
+temp1[[11]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.42))
+temp1[[12]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.44))
+temp1[[13]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.46))
+temp1[[14]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.472))
 temp1[[15]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.48))
 temp1[[16]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.489))
 temp1[[17]] <- rownames(subset.matrix(sil, sil[,1] == 1 & sil[,3] > 0.5))
 
 temp1[[18]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.13))
-temp1[[19]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.2))
-temp1[[20]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.3))
-temp1[[21]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.36))
-temp1[[22]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.4))
-temp1[[23]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.43))
-
-
-
-temp[[18]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.43))
-temp[[19]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.4 & sil[,3] <= 0.43))
-temp[[20]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.36 & sil[,3] <= 0.4))
-temp[[21]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.3 & sil[,3] <= 0.36))
-temp[[22]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.2 & sil[,3] <= 0.3))
-temp[[23]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.15 & sil[,3] <= 0.2))
-temp[[24]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.1 & sil[,3] <= 0.2))
-temp[[25]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] <= 0.1))
+temp1[[19]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.22))
+temp1[[20]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.29))
+temp1[[21]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.32))
+temp1[[22]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.34))
+temp1[[23]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.36))
+temp1[[24]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.38))
+temp1[[25]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.41))
+temp1[[26]] <- rownames(subset.matrix(sil, sil[,1] == 2 & sil[,3] > 0.43))
 
 #第二种不确定性计算方式（以信息熵来衡量）
 library(entropy)
@@ -102,5 +97,45 @@ Boston.uncertainty.dataframe <- as.data.frame(do.call(rbind, Boston.uncertaintyl
 
 #正态性检验
 Boston.SWtest <- apply(Boston.uncertainty.dataframe, 2, shapiro.test)
-
+Boston.SWtest
 shapiro.test(Boston.uncertainty.dataframe$dis)
+qqnorm(Boston.uncertainty.dataframe$crim, col = "blue", main = "")
+qqnorm(Boston.uncertainty.dataframe$rm, col = "blue", main = "")
+
+#相关分析
+library("Hmisc")
+Boston.corelationResult <- rcorr(as.matrix(Boston.uncertainty.dataframe))
+#数据重组
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    r  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+corrMatrix <- flattenCorrMatrix(Boston.corelationResult$r,Boston.corelationResult$P)
+ggplot(corrMatrix) + geom_point(aes(x = r, y = p))
+
+library(corrplot)
+par(mfrow=c(1,2))
+corrplot(Boston.corelationResult$r, method = "circle", type = "upper", order = "FPC",
+         p.mat = Boston.corelationResult$P, sig.level = 0.05, insig = "blank")
+
+#原数据的相关分析
+corrResultOfOriginalDATA <- rcorr(as.matrix(Boston))
+corrMatrixOfOriginalDATA <- flattenCorrMatrix(corrResultOfOriginalDATA$r, corrResultOfOriginalDATA$P)
+corrplot(corrResultOfOriginalDATA$r, method = "circle", type = "upper", order = "AOE",
+         p.mat = corrResultOfOriginalDATA$P, sig.level = 0.05, insig = "blank")
+
+rMatrix <- corrResultOfOriginalDATA$r
+rtemp <- apply(rMatrix, 2, sum)
+rtemp.sorted <- sort(rtemp, decreasing = T)
+orderNames <- names(rtemp.sorted)
+Boston.sorted <- Boston[orderNames]
+
+sorted.corrResultOfOriginalDATA <- rcorr(as.matrix(Boston.sorted))
+sorted.corrMatrixOfOriginalDATA <- flattenCorrMatrix(sorted.corrResultOfOriginalDATA$r, sorted.corrResultOfOriginalDATA$P)
+corrplot(sorted.corrResultOfOriginalDATA$r, method = "circle", type = "upper", order = "original",
+         p.mat = corrResultOfOriginalDATA$P, sig.level = 0.05, insig = "blank")
