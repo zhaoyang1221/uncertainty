15814b270d74813faea8aaa59a9d346cbb5f705d
zhaoyang1221 <164258541@qq.com>
2017-08-29 21:55:03 +0800

    不确定性数据和swtest的数据转json

--- printDataToJson.R
+++ printDataToJson.R
@@ -16,3 +16,17 @@ clusAvg <- data.frame(cluster = c(1,2), clusSilAvg = cars.scale.pamk$pamobject$s
 cars.sil_info <- list(silInfo = cars.sil, clusAvg = clusAvg, silAvg = as.numeric(cars.scale.pamk$pamobject$silinfo$avg.width))
 sil.scale.json <- toJSON(cars.sil_info, pretty = T)
 writeLines(sil.scale.json, "json/cars_sil.json")
+
+#打印uncertainty数据
+cars.scale.uncertainty.dataframe.plotJson <- toJSON(cars.scale.uncertainty.dataframe, pretty = T)
+writeLines(cars.scale.uncertainty.dataframe.plotJson, "json/cars_uncertainty_data.json")
+
+#打印正态检验结果
+library(data.table)
+SWtestlist <- rbindlist(SWtest,fill= TRUE)
+ 
+SWtest.dataframe <- data.frame(Attr = names(SWtest) ,SWtestlist)
+SWtest.dataframe <- subset(SWtest.dataframe, select = -data.name)
+SWtest.dataframe.json <- toJSON(SWtest.dataframe, pretty = T)
+writeLines(SWtest.dataframe.json, "json/cars_normaly_test.json")
+
--- uncertainty_cars_scale.R
+++ uncertainty_cars_scale.R
@@ -103,7 +103,8 @@ cars.scale.uncertainty.dataframe <- as.data.frame(do.call(rbind, scale.uncertain
 SWtest <- apply(cars.scale.uncertainty.dataframe, 2, shapiro.test)
 
 shapiro.test(cars.scale.uncertainty.dataframe$power)
-qqnorm(cars.scale.uncertainty.dataframe$economy, col = "blue", main = "")
+
+qqnormchart <- qqnorm(cars.scale.uncertainty.dataframe$economy, col = "blue", main = "") 
 
 qqline(cars.scale.uncertainty.dataframe$cylinders)
 hist(cars.scale.uncertainty.dataframe$displacement)
